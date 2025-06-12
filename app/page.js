0bc285a084b56779d2fb4e90e4b384cfeed068f1
Pukkok <krystalgggg@gmail.com>
2025-06-12 18:52:14 +0900

    [Feat] 넥스트 + 테일윈드 패키지 초기화

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-[#111] flex w-full h-screen items-center justify-center">
+      <h1 className="text-gray-50 text-lg font-bold">NEXT + TAILWIND</h1>
+    </div>
+  )
+}
