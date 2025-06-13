3d027fa861b0b8bffa8e4b30ce5a757016eb5f4d
Pukkok <krystalgggg@gmail.com>
2025-06-13 15:02:24 +0900

    [Feat] 페이지 link 설정

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
+  const linkData = [
+    {name : '폴더트리', href : '/folder-tree'}
+  ]
+
+
   return (
     <div className="bg-[#111] flex w-full h-screen items-center justify-center">
-      <h1 className="text-gray-50 text-lg font-bold">NEXT + TAILWIND</h1>
+      {linkData.map(item => (
+        <Link 
+          key={item.name}
+          className={`text-gray-200`}
+          href={item.href}>{item.name}
+        </Link>
+      ))}
     </div>
   )
 }
