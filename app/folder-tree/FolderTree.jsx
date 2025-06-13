120a165fa83592718517407c1fe59cb424e00b27
Pukkok <krystalgggg@gmail.com>
2025-06-14 00:08:40 +0900

    [Feat] context 연결

--- app/folder-tree/FolderTree.jsx
+++ app/folder-tree/FolderTree.jsx
@@ -1,17 +1,31 @@
 'use client'
 
+import { useState } from "react"
+import { treeData } from "./data"
 import TreeNode from "./TreeNode"
+import { DragContext } from "./contexts/dragContext"
 
-const FolderTree = ({ data }) => {
+const FolderTree = () => {
+  const [tree, setTree] = useState(treeData)
+  const [dragNode, setDragNode] = useState(null)
+  const [dropPoint, setDropPoint] = useState(null)
+
+  const contextValue = {
+    tree, setTree,
+    dragNode, setDragNode,
+    dropPoint, setDropPoint
+  }
 
   return (
-    <div>
-      {data.map(node => (
-        <TreeNode 
-          key={node.id} node={node} 
-        />
-      ))}
-    </div>
+    <DragContext.Provider value={contextValue}>
+      <div className="border-r h-screen w-xs">
+        {tree.map((node, idx) => (
+          <TreeNode 
+            key={node.id} node={node} isFirst={idx===0}
+          />
+        ))}
+      </div>
+    </DragContext.Provider>
   )
 }
 
